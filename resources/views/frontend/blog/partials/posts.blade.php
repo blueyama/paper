5cb3b343b94e67986d2a466f4f9caead4b6b88d5
Desmond Rush <blueyama@gmail.com>
2017-08-06 22:19:55 +0200

    WIP Posts now have thumbnails

--- resources/views/frontend/blog/partials/posts.blade.php
+++ resources/views/frontend/blog/partials/posts.blade.php
@@ -1,19 +1,63 @@
-@foreach ($posts as $post)
-    <div class="post-preview">
-        <h2 class="post-title">
-            <a href="{{ $post->url($tag) }}">{{ $post->title }}</a>
-        </h2>
-        <p class="post-meta">
-            {{ $post->published_at->diffForHumans() }} &#183; {{ $post->readingTime() }} MIN READ
-            <br>
-            @unless ($post->tags->isEmpty())
-                {!! implode(' ', $post->tagLinks()) !!}
-            @endunless
-        </p>
-        <p class="postSubtitle">
-            {{ str_limit($post->subtitle, config('blog.frontend_trim_width')) }}
-        </p>
-        <p style="font-size: 13px"><a href="{{ $post->url($tag) }}">READ MORE...</a></p>
+<?php
+
+// @todo move to helper function
+function isOdd($number)
+{
+    if($number & 1){
+        return true;
+    } else {
+        return false;
+    }
+}
+?>
+
+<style>
+    .post-preview {
+        border-bottom: 1px #ccc solid;
+        margin-bottom: 20px;
+    }
+    @media (min-width: 992px) {
+        .post-right-text {
+            text-align: right;
+        }
+
+        .post-right {
+            float: right;
+        }
+    }
+</style>
+
+@foreach ($posts as $key => $post)
+    <div class="row post-preview {{ isOdd($key) ? 'post-right-text' : '' }}">
+        <div class="col-xs-12 col-md-5 {{ isOdd($key) ? 'post-right' : '' }}">
+            <div style="height: 200px">
+                <a href="{{ $post->url($tag) }}">
+                    @if ($post->page_image)
+                        <figure style="width: 340px; height: 210px; background: url({{ asset($post->page_image)}}); background-size: cover;"></figure>
+                    @else
+                        <figure style="width: 340px; height: 210px; background: url('http://via.placeholder.com/340x210?text=No+Image'); background-size: cover;"></figure>
+                    @endif
+                </a>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-md-7">
+            <h2 class="post-title ">
+                <a href="{{ $post->url($tag) }}">{{ $post->title }}</a>
+            </h2>
+            <p class="post-meta">
+                {{ $post->published_at->diffForHumans() }} &#183; {{ $post->readingTime() }} MIN READ
+                <br>
+                @unless ($post->tags->isEmpty())
+                    {!! implode(' ', $post->tagLinks()) !!}
+                @endunless
+            </p>
+            <p class="postSubtitle">
+                {{ str_limit($post->subtitle, config('blog.frontend_trim_width')) }}
+            </p>
+            <p style="font-size: 13px">
+                <a href="{{ $post->url($tag) }}">READ MORE...</a>
+            </p>
+        </div>
     </div>
-    <hr>
 @endforeach
